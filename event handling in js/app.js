d6b5ec77703b3500e43829cc3c57e9e4ac22e678
zaul-2014 <zeenathsaee09@gmail.com>
2025-11-28 10:08:42 +0500

    Add files via upload
    The projects are in outside of others imgs vids.

---
+++ event handling in js/app.js
@@ -0,0 +1,10 @@
+function playAudio() {
+  const audio = document.getElementById("audio");
+  audio.play();
+}
+
+// Optional: showImage function if you want to toggle visibility
+function showImage() {
+  const img = document.querySelector(".scp-1055");
+  img.style.display = "block"; // or toggle with none/block
+}
